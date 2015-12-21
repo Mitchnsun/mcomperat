d74267df0f2a44e774dd68bf0dc22f566886ef9f
matthieu_comperat <matthieu.comperat@gmail.com>
2015-12-21 21:52:34 +0100

    [v1] Concat CSS & JS Files Uglify JS Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-lesslint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-concat-css');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	
 	// Configuration
 	grunt.initConfig({
@@ -25,10 +28,26 @@ module.exports = function(grunt) {
 					optimization : 2
 				},
 				files : {
-					"css/cv.css" : "less/cv.less" // destination file and source file
+					"css/layout.css" : "less/layout.less" // destination file and source file
 				}
 			}
 		},
+		concat_css: {
+      options: {
+        // Task-specific options go here. 
+      },
+      all: {
+        src: ["bower_components/pure/pure-min.css", "css/layout.css"],
+        dest: "dist/cv.css"
+      },
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/cv.min.js': ['bower_components/jquery/dist/jquery.min.js', 'js/cv.js']
+        }
+      }
+    },
 		jshint : {
 			all : ['Gruntfile.js', 'js/cv.js']
 		},
@@ -45,6 +64,6 @@ module.exports = function(grunt) {
 	});
 
 	// Tasks
-	grunt.registerTask('default',  ['less']);
+	grunt.registerTask('default',  ['less', 'uglify', 'concat_css']);
 	grunt.registerTask('analysis', ['plato', 'jshint', 'lesslint']);
 };
