fb76810e20a952c58edf10aa7cba54937f028dd3
matthieu_comperat <matthieu.comperat@gmail.com>
2016-01-27 19:36:34 +0100

    [v2] English by default

--- js/cv.js
+++ js/cv.js
@@ -2,6 +2,7 @@
 var SUPPORTED_LANG = ["fr", "en"];
 var DATA_PATH = "dist/assets/data/";
 var LANG = navigator.language || navigator.userLanguage;
+LANG = _.indexOf(SUPPORTED_LANG, LANG) === -1 ? "en" : LANG;
 
 $.getJSON(DATA_PATH + LANG + "/data.json", _.bind(templating, this));
 
