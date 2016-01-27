5c86113cc01b2b028395d6c4a5689299b8f1c968
matthieu_comperat <matthieu.comperat@gmail.com>
2016-01-27 16:53:58 +0100

    [v2] Create a template for the main page

--- js/cv.js
+++ js/cv.js
@@ -1,3 +1,8 @@
+var lang = navigator.language || navigator.userLanguage;
+
+var template = this.tmpl.cv;
+$('.content').html(template({}));
+
 /* DOM Ready and events function */
 $(document).ready(function(){
 	console.log('ready');
