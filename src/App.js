44374e0a522b195c9741a1114be1499a14a12912
Matthieu Comperat <matthieu.comperat@gmail.com>
2018-10-21 15:12:17 +0200

    [v3] Add Heading Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import Heading from './components/heading/Heading';
+import data from './assets/data/fr/data.json';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,11 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <Heading person={data.person}/>
+        <p className="App-container">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
