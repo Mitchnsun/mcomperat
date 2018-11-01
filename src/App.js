40c4bd9c733715a5163ea9a783327e107a334ce8
Matthieu Comperat <matthieu.comperat@gmail.com>
2018-11-01 17:44:15 +0100

    [v3] Use navigator language

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react';
 import Heading from './components/heading/Heading';
 import PostList from './components/post/PostList';
 import PostExtra from './components/post/PostExtra';
-import data from './assets/data/fr/data.json';
+import dataFR from './assets/data/fr/data.json';
+import dataEN from './assets/data/en/data.json';
 import logo from './logo.svg';
 import './App.css';
 
+const SUPPORTED_LANG = ['fr-FR', 'en-US'];
+const DATAS = {
+  'fr-FR': dataFR,
+  'en-US': dataEN
+};
+
 class App extends Component {
+  getData() {
+    const navigatorLang = window.location.hash.replace('#','') || navigator.language || navigator.userLanguage;
+    const lang = SUPPORTED_LANG.indexOf(navigatorLang) === -1 ? 'fr-FR' : navigatorLang;
+    return DATAS[lang];
+  }
+
   render() {
+    const data = this.getData();
+
     return (
       <div className="App">
         <div className="App-sidebar">
