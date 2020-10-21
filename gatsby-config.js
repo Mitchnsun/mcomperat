b9b37f084393799eb6426a3e18e772240a828c6f
Matthieu Compérat <matthieu.comperat@gmail.com>
2020-10-21 17:01:37 +0200

    [v4] Add Google Analytics Tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,11 @@ module.exports = {
         vendorPrefixes: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-gtag',
+      options: {
+        trackingId: 'UA-36979515-1',
+      },
+    },
   ],
 };
